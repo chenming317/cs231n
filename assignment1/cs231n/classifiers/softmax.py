d0ef2bd724ac51b1a9ad7094de77fd48e2ca8251
coky <chenming317@foxmail.com>
2017-09-04 14:51:47 +0800

    assignment1 softmax solution

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -30,7 +30,31 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num = X.shape[0]
+  num_class = W.shape[1]
+
+  for i in xrange(num):
+      scores = X[i].dot(W)
+
+      idx = np.argmax(scores)
+
+      scores -= scores[idx] # aviod big data
+
+      softmax_val = np.exp(scores)/np.sum(np.exp(scores))
+
+      loss += -np.log(softmax_val[y[i]])
+
+      for j in xrange(num_class):
+        if j == y[i]:
+            dW[:,j] += (softmax_val[j]-1)*X[i]
+        else:
+            dW[:,j] += softmax_val[j] * X[i]
+
+  loss /= num
+  loss += reg*np.sum(W*W)
+
+  dW /= num
+  dW += 2*reg*W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -54,7 +78,37 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num = X.shape[0]
+
+  scores = X.dot(W)
+
+  max_data = np.max(scores,axis=1)
+
+  scores -= max_data.reshape(num,1) # avoid big data
+
+  softmax_val = np.exp(scores) / np.sum(np.exp(scores),axis = 1).reshape(num,1)
+
+  class_idx = np.zeros(scores.shape)
+
+  class_idx[xrange(num),y] = 1
+
+  loss_val = softmax_val * class_idx
+
+  loss_val = loss_val.max(axis = 1)
+
+  loss_val = -np.log(loss_val)
+
+  loss = loss_val.sum()
+
+  softmax_val -= class_idx
+
+  dW = X.T.dot(softmax_val)
+
+  loss /= num
+  loss += reg*np.sum(W*W)
+
+  dW /= num
+  dW += 2*reg*W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
